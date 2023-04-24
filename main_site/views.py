56d62fe76f090c92fc8095d009daa30a0a23ba41
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-04-24 12:35:09 -0700

    Populated Employee table and displayed team members names and roles on the 'Teams' page

--- main_site/views.py
+++ main_site/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from .models import Employee
+
 
 def home(request):
     return render(request, "index.html")
 
 
 def team(request):
-    return render(request, "team.html")
+    employees = Employee.objects.all().values()
+    context = {'employees': employees}
+    return render(request, "team.html", context=context)
